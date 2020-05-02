84299104f5a05ae4faafa8c1e4e4bbcff4b91f41
Jonas A. Hultén <jonas@sldr.se>
2020-05-03 01:07:49 +0200

    Added encoder for second quin

--- zim/encode.py
+++ zim/encode.py
@@ -7,6 +7,8 @@ Class definition for the zim encoder, taking bytes-data and turning it into
 z-base-32 string.
 """
 
+from collections import namedtuple
+
 class EncoderAlreadyFinalizedException(Exception):
     pass
 
@@ -19,6 +21,7 @@ class Encoder():
             'ybndrfg8ejkmcpqxot1uwisza345h769'
         )
         self._is_finished = False
+        self._p_quin = namedtuple('ProcessedQuin', ['sym', 'rem'])
 
     def _make_alphabet(self, alphabet_string: str) -> dict:
         alphabet = {}
@@ -26,16 +29,37 @@ class Encoder():
             alphabet[i] = x
         return alphabet
 
+    def _render_first_quin(self, byte) -> tuple:
+        quin = byte >> 3
+        rem = (byte & 0b111) << 2
+        return self._p_quin(sym=self._alphabet.get(quin),
+                            rem=rem)
+
+    def _render_second_quin(self, byte, remainder) -> tuple:
+        sym = ''
+        quin = (byte >> 6) + remainder
+        sym += self._alphabet.get(quin)
+        quin = (byte >> 1) & 0b11111
+        rem = (byte & 0b1) << 4
+        sym += self._alphabet.get(quin)
+        return self._p_quin(sym=sym, rem=rem)
+
     def _render_quantum(self, quantum: bytearray) -> str:
         the_string = ''
-        data_byte = quantum[0]
-        quint = data_byte >> 3
-        remainder = (data_byte & 0b111) << 2
-        the_string += self._alphabet.get(quint)
+        p_quin = self._render_first_quin(quantum[0])
+        the_string += p_quin.sym
         if len(quantum) == 1:
-            the_string += self._alphabet.get(remainder)
+            the_string += self._alphabet.get(p_quin.rem)
             the_string += '======'
             return the_string
+        p_quin = self._render_second_quin(quantum[1], p_quin.rem)
+        the_string += p_quin.sym
+        if len(quantum) == 2:
+            the_string += self._alphabet.get(p_quin.rem)
+            the_string += '===='
+            return the_string
+
+
 
     def _consume(self):
         while len(self._byte_buffer) > 5:
