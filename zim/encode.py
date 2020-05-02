ee2feb93153fb1d834d5bffdf16c9f3a13c4e063
Jonas A. Hultén <jonas@sldr.se>
2020-05-03 00:04:52 +0200

    Added part of the encoding code

--- zim/encode.py
+++ zim/encode.py
@@ -26,8 +26,27 @@ class Encoder():
             alphabet[i] = x
         return alphabet
 
+    def _render_quantum(self, quantum: bytearray) -> str:
+        the_string = ''
+        data_byte = quantum[0]
+        quint = data_byte >> 3
+        remainder = (data_byte & 0b111) << 2
+        the_string += self._alphabet.get(quint)
+        if len(quantum) == 1:
+            the_string += self._alphabet.get(remainder)
+            the_string += '======'
+            return the_string
+
+    def _consume(self):
+        while len(self._byte_buffer) > 5:
+            quantum = self._byte_buffer[0:5]
+            del self._byte_buffer[0:5]
+            self._string += self._render_quantum(quantum)
+
     def update(self, data: bytes):
-        pass
+        self._byte_buffer.extend(bytearray(data))
+        self._consume()
 
     def finalize(self) -> str:
-        pass
+        self._is_finished = True
+        return self._string + self._render_quantum(self._byte_buffer)
