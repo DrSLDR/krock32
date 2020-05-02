9901de73a4816f21fbe44d881aa1a0e18900f1cb
Jonas A. Hultén <jonas@sldr.se>
2020-05-03 01:25:43 +0200

    Finished the encoder

--- zim/encode.py
+++ zim/encode.py
@@ -44,6 +44,29 @@ class Encoder():
         sym += self._alphabet.get(quin)
         return self._p_quin(sym=sym, rem=rem)
 
+    def _render_third_quin(self, byte, remainder) -> tuple:
+        quin = (byte >> 4) + remainder
+        rem = (byte & 0b1111) << 1
+        return self._p_quin(sym=self._alphabet.get(quin),
+                            rem=rem)
+
+    def _render_fourth_quin(self, byte, remainder) -> tuple:
+        sym = ''
+        quin = (byte >> 7) + remainder
+        sym += self._alphabet.get(quin)
+        quin = (byte >> 2) & 0b11111
+        sym += self._alphabet.get(quin)
+        rem = (byte & 0b11) << 3
+        return self._p_quin(sym=sym, rem=rem)
+
+    def _render_fifth_quin(self, byte, remainder) -> tuple:
+        sym = ''
+        quin = (byte >> 5) + remainder
+        sym += self._alphabet.get(quin)
+        quin = byte & 0b11111
+        sym += self._alphabet.get(quin)
+        return self._p_quin(sym=sym, rem=0)
+
     def _render_quantum(self, quantum: bytearray) -> str:
         the_string = ''
         p_quin = self._render_first_quin(quantum[0])
@@ -58,7 +81,20 @@ class Encoder():
             the_string += self._alphabet.get(p_quin.rem)
             the_string += '===='
             return the_string
-
+        p_quin = self._render_third_quin(quantum[2], p_quin.rem)
+        the_string += p_quin.sym
+        if len(quantum) == 3:
+            the_string += self._alphabet.get(p_quin.rem)
+            the_string += '==='
+            return the_string
+        p_quin = self._render_fourth_quin(quantum[3], p_quin.rem)
+        the_string += p_quin.sym
+        if len(quantum) == 4:
+            the_string += self._alphabet.get(p_quin.rem)
+            the_string += '=='
+            return the_string
+        p_quin = self._render_fifth_quin(quantum[4], p_quin.rem)
+        return the_string + p_quin.sym
 
 
     def _consume(self):
