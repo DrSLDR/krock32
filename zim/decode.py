8780b8b470cd1fb65985fd9df6a13875c72babb6
Jonas A. Hultén <jonas@sldr.se>
2020-05-03 14:06:28 +0200

    Added skeleton for alphabet generator for Decoder
    At this point, I realized I have been misguided and want to change
    alphabet to crockford's. As a result, I have to rename the project

--- zim/decode.py
+++ zim/decode.py
@@ -9,7 +9,25 @@ into bytes-data.
 
 class Decoder:
     def __init__(self, permissive=False, ignore_non_alphabet=True):
-        self._string_buffer = ''
-        self._bytearray = bytearray()
-        self._permissive = permissive
-        self._ignore_non_alphabet = ignore_non_alphabet
+        self._string_buffer: str = ''
+        self._bytearray: bytearray = bytearray()
+        self._permissive: bool = permissive
+        self._ignore_non_alphabet: bool = ignore_non_alphabet
+        self._alphabet = self._make_alphabet(
+            'ybndrfg8ejkmcpqxot1uwisza345h769',
+            permissive=self._permissive
+        )
+
+    def _make_alphabet(self, alphabet_string:str, permissive: bool) -> dict:
+        alphabet = {}
+        for i, x in enumerate(alphabet_string):
+            alphabet[x.lower()] = i
+            alphabet[x.upper()] = i
+        if permissive:
+            # Inject doubles for easier, more permissive parsing
+            alphabet['0'] = 16 # means o, O, 0 decodes to 16
+            alphabet['l'] = 18
+            alphabet['L'] = 18 # means 1, l, L decodes to 18
+            # i and I are also doubles of 1, but let's assume they can be
+            # read properly
+        return alphabet
