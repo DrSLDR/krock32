52505ec5c6b5b7480d06886f54ea40e68e5f47f0
Jonas A. Hultén <jonas@sldr.se>
2020-05-10 21:37:37 +0200

    Starting decoder logic

--- krock32/decode.py
+++ krock32/decode.py
@@ -7,6 +7,8 @@ Class definition for the Krock32 decoder, taking a Crockford Base32 string
 and turning it into bytes-data.
 """
 
+from collections import namedtuple
+
 
 class DecoderAlreadyFinalizedException(Exception):
     pass
@@ -23,6 +25,7 @@ class Decoder:
             strict=self._strict
         )
         self._is_finished = False
+        self._p_sym = namedtuple('ProcessedSymbol', ['byte', 'rem'])
 
     def _make_alphabet(self, alphabet_string: str, strict: bool) -> dict:
         alphabet = {}
@@ -36,11 +39,32 @@ class Decoder:
              alphabet['L'], alphabet['l']) = 1, 1, 1, 1
         return alphabet
 
+    def _decode_first_symbol(self, symbol: str) -> bytes:
+        return self._p_sym(byte=self._alphabet.get(symbol), rem=0)
+
+    def _decode_quantum(self, quantum: str) -> bytearray:
+        buffer = bytearray()
+        p_sym = self._decode_first_symbol(quantum[0])
+        if len(quantum) == 1:
+            return buffer.append(p_sym.byte << 3)
+
+    def _consume(self):
+        while len(self._string_buffer) > 8:
+            quantum: str = self._string_buffer[0:8]
+            self._string_buffer = self._string_buffer[8:]
+            self._bytearray.extend(self._decode_quantum(quantum))
+
     def update(self, string: str):
         if self._is_finished:
             raise DecoderAlreadyFinalizedException
+        self._string_buffer += string
+        self._consume()
 
     def finalize(self) -> bytes:
         if self._is_finished:
             raise DecoderAlreadyFinalizedException
         self._is_finished = True
+        decoding: bytearray = self._bytearray.append(
+                                (self._decode_quantum(self._string_buffer)
+                                 if len(self._string_buffer) > 0 else b''))
+        return bytes(decoding)
