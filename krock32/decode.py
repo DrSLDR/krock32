696bfca1abefe2a9c0c92e1107a5cc5c0f16ddea
Jonas A. Hultén <jonas@sldr.se>
2020-05-10 15:31:41 +0200

    Added proper alphabet generation for the decoder

--- krock32/decode.py
+++ krock32/decode.py
@@ -7,27 +7,26 @@ Class definition for the Krock32 decoder, taking a Crockford Base32 string
 and turning it into bytes-data.
 """
 
+
 class Decoder:
-    def __init__(self, permissive=False, ignore_non_alphabet=True):
+    def __init__(self, strict=False, ignore_non_alphabet=True):
         self._string_buffer: str = ''
         self._bytearray: bytearray = bytearray()
-        self._permissive: bool = permissive
+        self._strict: bool = strict
         self._ignore_non_alphabet: bool = ignore_non_alphabet
         self._alphabet = self._make_alphabet(
-            'ybndrfg8ejkmcpqxot1uwisza345h769',
-            permissive=self._permissive
+            '0123456789ABCDEFGHJKMNPQRSTVWXYZ*~$=U',
+            strict=self._strict
         )
 
-    def _make_alphabet(self, alphabet_string:str, permissive: bool) -> dict:
+    def _make_alphabet(self, alphabet_string: str, strict: bool) -> dict:
         alphabet = {}
         for i, x in enumerate(alphabet_string):
-            alphabet[x.lower()] = i
             alphabet[x.upper()] = i
-        if permissive:
-            # Inject doubles for easier, more permissive parsing
-            alphabet['0'] = 16 # means o, O, 0 decodes to 16
-            alphabet['l'] = 18
-            alphabet['L'] = 18 # means 1, l, L decodes to 18
-            # i and I are also doubles of 1, but let's assume they can be
-            # read properly
+            if not strict:
+                alphabet[x.lower()] = i
+        if not strict:
+            alphabet['O'], alphabet['o'] = 0, 0
+            (alphabet['I'], alphabet['i'],
+             alphabet['L'], alphabet['l']) = 1, 1, 1, 1
         return alphabet
