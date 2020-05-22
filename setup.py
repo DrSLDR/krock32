ac142aa8debc85abe43d67e567365d71c34f9113
Jonas A. Hultén <jonas@sldr.se>
2020-05-22 18:30:42 +0200

    Added most of the setup.py file, registered that to bumpversion

---
+++ setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / 'README.md').read_text()
+
+setup(
+    name='krock32',
+    version='0.0.1',
+    description='Base32 encoder/decoder using Crockford\'s alphabet',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/DrSLDR/krock32',
+    author='Jonas A. Hultén',
+    author_email='sldr@sldr.se',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8'
+    ],
+    packages=['krock32'],
+    include_package_data=True
+)
