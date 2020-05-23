bc10721b94a4b0250105054326a400171ebde3ae
Jonas A. Hultén <jonas@sldr.se>
2020-05-23 21:21:13 +0200

    Bump version: 0.1.0 → 0.1.1

--- krock32/__init__.py
+++ krock32/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 
 """
-Krock32 module 0.1.0
+Krock32 module 0.1.1
 
 Implementation of Crockford's base32 alphabet encoding, decoding,
 and pretty printing
@@ -11,4 +11,4 @@ and pretty printing
 from krock32.decode import Decoder
 from krock32.encode import Encoder
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
